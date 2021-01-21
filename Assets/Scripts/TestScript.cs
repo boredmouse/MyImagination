9da8157c12dc12733a97abaeaa3bcd183a9aa518
boredmouse <690573478@qq.com>
2021-01-21 14:05:48 +0800

    TMP

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TestScript : MonoBehaviour
+{
+    public Text FirstText;
+
+    public TMP_Text text1;
+    public GameObject a;
+    // Start is called before the first frame update
+    void Start()
+    {
+        FirstText.text = "Welocome Home!";
+        text1.text = "what>";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
