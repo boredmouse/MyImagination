bc32cdbe2f44184674d8da22b8a44e14d1513182
boredmouse <690573478@qq.com>
2021-01-28 17:30:59 +0800

    加入趴下状态

--- Assets/Scripts/Controller/PlayerController.cs
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -11,7 +11,9 @@ public class PlayerController : MonoBehaviour
         Idle = 0,
         Walk = 1,
         Jump = 2,
-        Down = 3
+        Down = 3,
+        DownWalk = 4,
+        DownIdle = 5
     };
     public float Speed = 0.1f;
     public Transform modelParent;
@@ -54,28 +56,58 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
+        //前进
+        if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A) && this.state != PalyerState.Idle)
         {
             this.transform.Translate(Speed, 0, 0);
         }
-        if (Input.GetKeyDown(KeyCode.D) && !Input.GetKey(KeyCode.A) && this.state != PalyerState.Jump)
+        if (Input.GetKeyDown(KeyCode.D) && !Input.GetKey(KeyCode.A) && this.state != PalyerState.Jump && this.state != PalyerState.Down)
         {
-            this.SetState(PalyerState.Walk);
+            if (this.state == PalyerState.Idle)
+            {
+                this.SetState(PalyerState.Walk);
+            }
+            else if (this.state == PalyerState.DownIdle)
+            {
+                this.SetState(PalyerState.DownWalk);
+            }
         }
-        if (Input.GetKeyUp(KeyCode.D) && !Input.GetKey(KeyCode.A) && this.state == PalyerState.Walk)
+        if (Input.GetKeyUp(KeyCode.D) && !Input.GetKey(KeyCode.A))
         {
-            this.SetState(PalyerState.Idle);
+            if (this.state == PalyerState.Walk)
+            {
+                this.SetState(PalyerState.Idle);
+            }
+            else if (this.state == PalyerState.DownWalk)
+            {
+                this.SetState(PalyerState.DownIdle);
+            }
         }
-        if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+        //后退
+        if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D) && this.state != PalyerState.Idle)
         {
             if (this.transform.position.x - Camera.main.transform.position.x > cameraOffset.x - 2)
             {
                 this.transform.Translate(-Speed, 0, 0);
             }
         }
-        if (Input.GetKeyDown(KeyCode.Space) && this.state != PalyerState.Down)
+        if (Input.GetKeyDown(KeyCode.A) && !Input.GetKey(KeyCode.D) && this.state != PalyerState.Jump)
+        {
+            this.SetState(PalyerState.Walk);
+        }
+        if (Input.GetKeyUp(KeyCode.A) && !Input.GetKey(KeyCode.D) && this.state == PalyerState.Walk)
+        {
+            this.SetState(PalyerState.Idle);
+        }
+        //跳跃
+        if (Input.GetKeyDown(KeyCode.Space) && this.state != PalyerState.Jump)
         {
             this.SetState(PalyerState.Jump, 0.6f);
+        }
+        //下蹲
+        if (Input.GetKeyDown(KeyCode.S) && (this.state == PalyerState.Walk || this.state == PalyerState.Idle))
+        {
+            this.SetState(PalyerState.Down, 0.5f);
         }
     }
 
@@ -113,6 +145,17 @@ public class PlayerController : MonoBehaviour
                 this.SetStateAndAnim(PalyerState.Idle);
             }
         }
+        else if (state == PalyerState.Down)
+        {
+            if (Input.GetKey(KeyCode.D))
+            {
+                this.SetStateAndAnim(PalyerState.DownWalk);
+            }
+            else
+            {
+                this.SetStateAndAnim(PalyerState.DownIdle);
+            }
+        }
     }
 
     void SetStateAndAnim(PalyerState state)
@@ -139,9 +182,33 @@ public class PlayerController : MonoBehaviour
         else if (state == PalyerState.Jump)
         {
             this.animationCom.Play("jump");
-            if (this.modelAnimator!=null)
+            if (this.modelAnimator != null)
             {
-            this.modelAnimator.Play("jump");
+                this.modelAnimator.Play("jump");
+            }
+        }
+        else if (state == PalyerState.Down)
+        {
+            //this.animationCom.Play("down");
+            if (this.modelAnimator != null)
+            {
+                this.modelAnimator.Play("down");
+            }
+        }
+        else if (state == PalyerState.DownWalk)
+        {
+            //this.animationCom.Play("downwalk");
+            if (this.modelAnimator != null)
+            {
+                this.modelAnimator.Play("downwalk");
+            }
+        }
+        else if (state == PalyerState.DownIdle)
+        {
+            //this.animationCom.Play("downidle");
+            if (this.modelAnimator != null)
+            {
+                this.modelAnimator.Play("downidle");
             }
         }
     }
@@ -152,7 +219,8 @@ public class PlayerController : MonoBehaviour
     void OnGetCloth(string id, CommonEnum.PartType part)
     {
         this.bodyParts[part] = id;
-        this.LoadHeroModel(this.GetModelPathByBodyParts());
+        string path = this.GetModelPathByBodyParts();
+        StartCoroutine(WaitForChangeCloth(path));
     }
 
     string GetModelPathByBodyParts()
@@ -164,11 +232,11 @@ public class PlayerController : MonoBehaviour
         + this.bodyParts[CommonEnum.PartType.shooes]
         ;
         var tableItem = ModelResourceConfig.GetConfigByID(partIdStr);
-        if(tableItem == null)
+        if (tableItem == null)
         {
-            Debug.LogError("ModelResourceConfig找不到id："+partIdStr);
+            Debug.LogError("ModelResourceConfig找不到id：" + partIdStr);
         }
-        var path =tableItem.Path;
+        var path = tableItem.Path;
         return path;
     }
     void LoadHeroModel(string path)
@@ -184,12 +252,18 @@ public class PlayerController : MonoBehaviour
         this.modelAnimator = go.GetComponent<Animator>();
         this.aseModel = go;
     }
+
+    IEnumerator WaitForChangeCloth(string path)
+    {
+        while (this.state != PalyerState.Idle && this.state != PalyerState.Walk)
+        {
+            yield return new WaitForSeconds(0.1f);
+        }
+        this.LoadHeroModel(path);
+        //todo 换装状态 暂时用idle代替
+        this.SetState(PalyerState.Idle);
+    }
     #endregion
 }
 
-/*public class BodyPart{
-    public int id = 0;
-    public CommonEnum.PartType part = CommonEnum.PartType.body;
-    
-}*/
 
